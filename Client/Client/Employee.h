70ffb4926ab0bc0eac79d7fc0f3514368beac163
Krainyaya Maria <rizhik_masha23@mail,ru>
2019-04-07 14:46:15 +0300

    client-server

--- Client/Client/Employee.h
+++ Client/Client/Employee.h
@@ -3,9 +3,9 @@
 #include <fstream>
 #include <cstdlib>
 #include <iomanip>
-//#include "IDGenerator.h"
-//#include "Encryptor.cpp"
-//#include "Role.h"
+#include "IDGenerator.h"
+#include "Encryptor.cpp"
+#include "Role.h"
 using namespace std;
 using std::string;
 #if defined(max)
@@ -16,37 +16,44 @@ class Employee {
 private:
 
 	int ID;
+	string name;
 	string login;
 	string password;
-	//Role role;
+	Role role;
 
 public:
 	Employee() {
+		name = "";
 		login = "";
 		password = "";
 	};
 
-	Employee(int id, string l, string p, /*Role role*/) {
+	Employee(int id,string n, string l, string p, Role role) {
 		ID = id;
+		name = n;
 		login = l;
 		password = p;
-		/*this->role = role*/;
+		this->role = role;
 	}
 
 	Employee(const Employee &user) {
 		ID = user.ID;
+		name = user.name;
 		login = user.login;
 		password = user.password;
-		/*role = user.role*/;
+		role = user.role;
 	}
 	virtual ~Employee() {}
-	//void toString();
+	void setName(string name);
+	void readName();
+	string getName();
+	void toString();
 	void setLog();
 	void setPas();
-	//void setRole(Role role);
+	void setRole(Role role);
 	int getID();
 	void setID(int id);
 	string getLogin();
 	string getPassword();
-	//Role getRole();
+	Role getRole();
 };
---
+++ Client/Client/Role.h
@@ -0,0 +1,6 @@
+#pragma once
+#pragma once
+enum Role {
+	SELLER,
+	PARTICIPANT
+};
